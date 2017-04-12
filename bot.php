cc117a814094ecf5a4b191bb0ca025bb004086a4
grit0 <krit405f@gmail.com>
2017-04-12 11:38:51 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = 'Ix6Et3oEImOG2kDfvI/ZT+8S2x+JDNJRJBX1oNBA6UKWntWtLpa/wOfrq7EFvCd+tbN5Cta3qgSB9zE9o+jjJf3Wolc1E25lM/KUbBozktDcdfdfaBSBDc2T+kVmeZJ0cXkRQRzq4Mmc4QkPj5owXAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');

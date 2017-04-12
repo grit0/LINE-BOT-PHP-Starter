5404e7124b761f807c8caaa1fd7ffe7a56ef9d74
grit0 <krit405f@gmail.com>
2017-04-12 11:46:16 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,10 +17,13 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
+			if($text="ดีจ้า")
+				$messages = ['type' => 'text','text' => "สวัสดีดีครัซ..";
+			else{
 			$messages = [
 				'type' => 'text',
 				'text' => $text
-			];
+			];}
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
